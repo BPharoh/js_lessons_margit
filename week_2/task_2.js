d4fff67d3cb85ed59c7e22496399e1f2085fba22
student <student@BC-minimac004s-Mac-mini.local>
2022-08-16 15:00:40 +0300

    solutions

---
+++ week_2/task_2.js
@@ -0,0 +1,37 @@
+// Guess what answers you would get if you ran this in the Javascript Console.
+//Once you have an answer to the questions then check them by copying them and running it in the console yourself line by line 
+
+
+//Evaluate the below:
+something! // This will add the two numbers to 68
+3 + "65" //68
+7 - "3" //Perform substraction - 4
+15 % 5 //Give remainder - 0
+5 % 15 //Give remainder in decimal form
+"Java" + "Script" // Produce the word Javascript
+"Java" - "Script" //Give me nothing
+" " + " " // Gives me a blank space
+" " + 0  // Gives me the number zero
+true + true //True (1)
+true + false //Gives me 1
+false + true // Gives me 1
+false - true // Gives negative 1
+3 - 4 // gives negative 1
+
+
+//Evaluate the below comparisons:
+4 >= 2 // true
+6 === 2 // false
+3 <= 1 //false
+1 != 1  //false
+"A" > "B" //false
+"B" < "C" //true
+"a" > "A" //true
+"b" < "A" //false
+true === false //false
+true != true //false
+
+
+// Make the string: 
+// "Go To Helsinki Business College," they said. "It'll Will Be "Fun"," they said.
+// by using the + sign:
---
+++ week_2/task_ms/main.js
@@ -0,0 +1,21 @@
+let age;
+prompt('What is your age');
+
+    if (age < 18) {
+        console.log('Too Young');
+    }
+else
+    if (age < 27) {
+        console.log('Right age for military service');
+    } else {
+
+    if (age < 41) {
+        console.log('You are in Reserve'); 
+
+      }  else {
+    if (age < 55) {
+        console.log('You are in backup reserve');
+    
+} else {
+        console.log('too aged');
+}
