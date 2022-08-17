a5963d7e059991bcdb6506f7b385436a1fc1751f
student <student@BC-minimac004s-Mac-mini.local>
2022-08-17 14:58:33 +0300

    added loop task and more updates

--- week_2/Random_Numbers/main.js
+++ week_2/Random_Numbers/main.js
@@ -1,8 +1,8 @@
 const checkNumbers = () => { 
 
-const num1 = prompt('Input first number');
-const num2 = prompt('Input second number');
-const num3 = prompt('Input third number');
+const num1 = Number(prompt('Input first number'));
+const num2 = Number(prompt('Input second number'));
+const num3 = Number(prompt('Input third number'));
 
 addition = num1 + num2 + num3;
 multiply = num1 * num2 * num3;
@@ -10,7 +10,7 @@ multiply = num1 * num2 * num3;
 if (num1 >= 0 && num2 >= 0 && num3 >=0) {
     return console.log(`This is sum ${addition} and this is multiplication ${multiply}`);
 }   else if (num1 <=0 || num1>=0 && num2 <=0 || num2>=0 && num3<=0 || num3>=0) {
-    return console.log(`The sum is ${addition}`);
+    return console.log(`The sum is ${addition} and this is multiplication ${multiply}`);
 }   else if (num1 < 0 && num2<0 && num3<0) {
     return console.log('These are negatives');
 }
