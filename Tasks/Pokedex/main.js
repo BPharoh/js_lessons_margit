4a37cd1e6b92d4f7f05f292ea90fb16b5d09114e
Otulugbu Alexander <s2200742@Alexander-Air.wlan>
2022-10-22 23:43:35 +0300

    Updated Pokedex

--- Tasks/Pokedex/main.js
+++ Tasks/Pokedex/main.js
@@ -1,30 +1,40 @@
 const content = document.querySelector('.container');
+const search = document.querySelector('#game-search');
+const generation = document.querySelectorAll('input[name="gen"]')
 let pokeData = [];
 
 
 const fetchData =  () => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=40&offset=0/')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=150&offset=0/')
     .then((response) => response.json())
-    .then((pokemon) => { 
-        const fetches = pokemon.results.map((item) => {
-            return fetch(item.url).then(res => res.json());
+    .then((data) => { 
+        const fetches = data.results.map((item) => {
+            return fetch(item.url).then(res => res.json())
+            .then((data) => {
+                return {
+                id: data.id,
+                name: data.name,
+                img: data.sprites.other["official-artwork"].front_default,   
+                type: data.types.map((type) => type.type.name).join(', '),
+              };
+            }); 
         });
     Promise.all(fetches).then((res) => { 
     pokeData = res;
-    pokeCards(res);
+    pokeCards();
+    console.log(pokeData);
     });
-    console.log(pokemon);
-});
+    });
 };
 
 
-const pokeCards = (pokemon) => {
-    console.log(pokemon);
+const pokeCards = () => {
+    // console.log(pokemon);
     const cards = pokeData
     .map((pokemon) => {
         return ` <div class="cards image">
         <h2>${pokemon.id}. ${pokemon.name}</h2>
-        <img src=${pokemon.sprites.other["official-artwork"].front_default} alt="${pokemon.name}"/>
+        <img src=${pokemon.img} alt="${pokemon.name}"/>
         <p> Type: ${pokemon.type}</p>
         </div>`;
         })
@@ -35,4 +45,6 @@ content.innerHTML = cards;
 };
 
 fetchData();
+
+
 
